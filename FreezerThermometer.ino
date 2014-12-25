0506d5eb54b63a451a4573cf188eb0897064536b
imabug <eugenemah@gmail.com>
2014-12-25 08:20:17 -0500

    Moved variable initializations out of setup(). Apparently you're not supposed to do that.

--- FreezerThermometer.ino
+++ FreezerThermometer.ino
@@ -21,17 +21,22 @@ Freezer thermometer
 // For the TMP36 temperature sensor
 #define aref_voltage 3.3         // we tie 3.3V to ARef and measure it with a multimeter!
 
-void setup() {
-  // analog pin for the TMP36 Vout pin
-  // resolution is 10 mV/deg C with a 500 mV offset to allow for
-  // negative temperatures
-  int tempPin = 1;
-  int tempReading;
+// analog pin for the TMP36 Vout pin
+// resolution is 10 mV/deg C with a 500 mV offset to allow for
+// negative temperatures
+int tempPin = 1;
+int tempReading;
+int delayVal = 5*60*1000;    // delay interval before reading the TMP36 in ms
+float voltage;
+float temperatureC;
 
+// Get an LCD object
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+
+void setup() {
+  
   // Debugging output
   // Serial.begin(9600);
-  // Get an LCD object
-  Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
@@ -45,9 +50,9 @@ void setup() {
 void loop(void) {
   tempReading = analogRead(tempPin);
   // converting that reading to voltage, which is based off the reference voltage
-  float voltage = (tempReading * aref_voltage)/1024.0;
+  voltage = (tempReading * aref_voltage)/1024.0;
   // now calculate the temperature from the measured voltage
-  float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+  temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
   //to degrees ((volatge - 500mV) times 100)
 
   // Display the current temperature on the second line of LCD display
@@ -56,6 +61,6 @@ void loop(void) {
   lcd.print(" degC");
 
   // Wait 5 minutes before making the next measurement
-  delay(5*60*1000);
+  delay(delayVal);
 }
 
