654a40aba34481ca5ec243da561e3911a6dc58dd
Eugene Mah <eugenemah@gmail.com>
2015-01-03 06:44:41 -0500

    Start implementing a menu via buttons

--- FreezerThermometer.ino
+++ FreezerThermometer.ino
@@ -30,10 +30,17 @@ Freezer thermometer
 int tempPin = 1;
 unsigned long dispTime = 30*1000;     // time to leave the LCD on for
 unsigned long prevMillis = 0;
-float currTemp;
-float minTemp;
-float maxTemp;
-int buttons;
+int tempInterval = 1;    // Time interval between readings
+float currTemp = 0;
+float minTemp = 0;
+float maxTemp = 0;
+float hiAlertTemp = -10.0;
+float loAlertTemp = -25.0;
+float tempHistory[10]
+uint8_t buttons;
+// Create arrays for 5 timers
+unsigned long timer[5];
+byte timerState[5];
 
 // Get an LCD object
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
@@ -108,8 +115,8 @@ void showTemp(float t1, float t2, float t3) {
   lcd.print(t3,1);
 }
 
-void loop(void) {
-  unsigned long currMillis;
+void loop(void) {  
+  unsigned long currMillis = millis();
   
   if (buttons = lcd.readButtons()) {
     if (buttons & BUTTON_SELECT) {
@@ -118,12 +125,68 @@ void loop(void) {
       if (currTemp > maxTemp) maxTemp = currTemp;
       showTemp(currTemp, minTemp, maxTemp);
     }
+    if (buttons & BUTTON_UP) {
+      // Set high alert temp using the up/down buttons
+    }
+    if (buttons & BUTTON_DOWN) {
+      // Set low alert temp using the up/down buttons
+    }
+    if ((buttons & BUTTON_LEFT) || (buttons & BUTTON_RIGHT)) {
+      // Scroll through temperature history
+    }
   }
-  currMillis = millis();      
-  if ((currMillis - prevMillis) > dispTime) {
-    prevMillis = currMillis;
-    lcd.noDisplay();            // Turn the display off
-    lcd.setBacklight(BLACK);    // turn the backlight off 
+
+  if (delayMinutes(0,1)) {
+    currTemp = getTemp();    // Get a temperature reading every minute
+    if (currTemp < minTemp) minTemp = currTemp;
+    if (currTemp > maxTemp) maxTemp = currTemp;
+    if (currTemp > hiAlertTemp) {
+      lcd.setBacklight(RED);
+    }
+//    lcd.noDisplay();            // Turn the display off
+//    lcd.setBacklight(BLACK);    // turn the backlight off 
+  }
+}
+
+/*  Delay without "delay()"
+ *   Chad Higgins 2012
+ *   Demonstates how to use write a delay that is like "blink without delay" but is used much like "delay()"
+ * http://playground.arduino.cc/Code/DelaySeconds
+ */
+int delayHours(byte timerNumber,unsigned long delaytimeH){    
+  //Here we make it easy to set a delay in Hours
+  delayMilliSeconds(timerNumber,delaytimeH*1000*60*60);
+}
+int delayMinutes(byte timerNumber,unsigned long delaytimeM){    
+  //Here we make it easy to set a delay in Minutes
+  delayMilliSeconds(timerNumber,delaytimeM*1000*60);
+}
+int delaySeconds(byte timerNumber,unsigned long delaytimeS){    
+  //Here we make it easy to set a delay in Seconds
+  delayMilliSeconds(timerNumber,delaytimeS*1000);
+}
+
+int delayMilliSeconds(int timerNumber,unsigned long delaytime){
+  unsigned long timeTaken;
+  if (timerState[timerNumber]==0){    
+    //If the timer has been reset (which means timer (state ==0) then save millis() to the same number timer, 
+    timer[timerNumber]=millis();
+    timerState[timerNumber]=1;      //now we want mark this timer "not reset" so that next time through it doesn't get changed.
+  }
+  if (millis()> timer[timerNumber]){
+    timeTaken=millis()+1-timer[timerNumber];    //here we see how much time has passed
+  }
+  else{
+    timeTaken=millis()+2+(4294967295-timer[timerNumber]);    //if the timer rolled over (more than 48 days passed)then this line accounts for that
+  }
+  if (timeTaken>=delaytime) {          
+    //here we make it easy to wrap the code we want to time in an "IF" statement, if not then it isn't and so doesn't get run.
+     timerState[timerNumber]=0;  //once enough time has passed the timer is marked reset.
+     return 1;                          //if enough time has passed the "IF" statement is true
+  }
+  else {                               
+    //if enough time has not passed then the "if" statement will not be true.
+    return 0;
   }
 }
 
